6ec496925a61feec74f10cd122bb732533520fb1
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-11-18 13:58:18 +0530

    User experience updates

---
+++ internship/src/components/layouts/AnimatedFrame.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
+const AnimatedFrame = ({ children }) => {
+  const colours = ["#ff0000", "#ffde59", "#1800ad", "#ff0000"];
+  const borderDiv = useRef(null)
+
+  useEffect(() => {
+        let i = 0;
+        setInterval(() => {
+          borderDiv.current.style.borderImageSource = `linear-gradient(to right, ${colours[i % 4]}, ${colours[(i + 1) % 4]}, ${colours[(i + 2) % 4]}, ${colours[(i + 3) % 4]})`;
+          i++;
+        }, 15000);
+  });
+
+  return (
+    <div className="relative max-w-80 min-h-50 rounded-2xl overflow-hidden h-full p-1">
+      <motion.div
+        ref={borderDiv}
+        className="absolute inset-0 rounded-lg"
+        transition={{ duration: 8, repeat: Infinity, ease: "linear" }}
+        style={{
+          border: "2px dotted",
+          borderImageSlice: 1,
+          borderImageSource: `linear-gradient(to right, #ff0000, #ffde59, #1800ad, #ff0000)`,
+        }}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default AnimatedFrame;
--- internship/src/pages/Career.jsx
+++ internship/src/pages/Career.jsx
@@ -10,7 +10,7 @@ import {
   FaFolderOpen,
 } from "react-icons/fa";
 
-import ContactUs from './ContactUs'
+import ContactUs from "./ContactUs";
 
 const items = [
   {
@@ -38,7 +38,7 @@ const items = [
     link: "/",
   },
 ];
- 
+
 const Career = () => {
   return (
     <section className="w-full min-h-screen mx-auto mt-2 text-white overflow-hidden">
@@ -50,28 +50,28 @@ const Career = () => {
         transition={{ duration: 0.8 }}
       ></motion.div>
       <motion.div
-        className="min-h-70 h-full max-w-4xl mx-auto text-center p-10"
+        className="min-h-70 h-full max-w-4xl mx-auto text-center p-10 font-subtitle font tracking-wider font-medium"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, delay: 0.2 }}
+        transition={{ duration: 0.8 }}
       >
         <motion.h4
-          className="text-lg md:text-3xl font-semibold"
+          className="text-lg md:text-3xl font "
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
+          transition={{ duration: 0.6 }}
         >
-          We are <span className="text-vivid-red font-bold">RECRUITING</span>
+          We are <span className="text-vivid-red font">RECRUITING</span>
         </motion.h4>
         <motion.h2
-          className="text-3xl md:text-5xl font-semibold"
+          className="text-3xl md:text-5xl font"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.6 }}
+          transition={{ duration: 0.6 }}
         >
           creative & awesome people
         </motion.h2>
-        <p className="text-lg font-normal my-10">
+        <p className="text-lg font-normal my-10 font-poppins">
           Join our Internship Program and fast-track your growth...! <br />
           We’re onboarding smart, creative learners ready to upskill,
           collaborate, and build real-world projects with{" "}
@@ -82,19 +82,19 @@ const Career = () => {
         </p>
       </motion.div>
       <motion.div
-        className="min-h-screen max-w-screen p-10 text-black bg-cover"
+        className="min-h-screen max-w-screen p-3 md:p-10 text-black bg-cover"
         style={{ backgroundImage: ` url(${paper})` }}
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.8, delay: 0.2 }}
+        viewport={{ once: false }}
+        transition={{ duration: 0.8 }}
       >
         <motion.h1
-          className="font-bold text-4xl"
+          className="text-4xl font-subtitle font tracking-wider font-medium"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.6, delay: 0.4 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.6 }}
         >
           Our Learning{" "}
           <span className="text-vivid-indigo font-bold">Programs</span>
@@ -103,47 +103,53 @@ const Career = () => {
           className="text-lg font-medium my-5"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true}}
-          transition={{ duration: 0.6, delay: 0.6 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.6 }}
         >
           Short, practical courses designed to help you learn fast, build real
           skills, and grow with hands-on experience.
         </motion.p>
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-3 mx-auto max-w-screen-xl gap-2"
+          className="grid grid-cols-2 md:grid-cols-3 mx-auto max-w-screen-xl gap-y-5 gap-x-3"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.8, delay: 0.8 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.8 }}
         >
-          {courses.map((course) => {
+          {courses.map((course, idx) => {
+            const x = idx % 2 === 0 ? -10 : 10;
             return (
-              <div
+              <motion.div
+                initial={{ x: x }}
+                whileInView={{ x: 0 }}
+                whileHover={{ scale: 1.04 }}
+                transition={{ duration: 0.4}}
+                viewport={{amount:0.4}}
                 key={course.id}
-                className="max-w-80 min-h-50 h-full grid grid-rows-2 rounded-2xl overflow-hidden border ml-auto md:mx-auto"
+                className="max-w-80 min-h-50 h-full grid grid-rows-2 rounded-2xl overflow-hidden border ml-auto md:mx-auto ring-1 ring-offset-vivid-red shadow-vivid-indigo"
               >
                 <img src="/" className="row-span-0 bg-red-50" alt="" />
                 <div
-                  className={`p-3 grid grid-rows-0 min-h-40 ${course.gradient}`}
+                  className={`p-3 grid grid-rows-0 min-h-30 md:min-h-40 ${course.gradient}`}
                 >
                   <h3 className="row-span-0 font-bold text-light-yellow">
                     {course.title}
                   </h3>
-                  <p className="row-span-0 text-white font-light text-sm">
+                  <p className="row-span-0 text-white font-light text-xs md:text-sm">
                     {course.description}
                   </p>
                   <div className="row-span-0 ml-auto">
                     {Object.keys(course.actions).map((btn) => (
                       <button
                         key={btn}
-                        className="p-1 text-sm font-semibold rounded-xl ml-2 text-black capitalize bg-white"
+                        className="p-1 text-xs mt-2 inline-block md:text-sm font-semibold rounded-xl ml-2 text-black capitalize bg-white"
                       >
                         {btn}
                       </button>
                     ))}
                   </div>
                 </div>
-              </div>
+              </motion.div>
             );
           })}
         </motion.div>
@@ -152,34 +158,34 @@ const Career = () => {
         className="text-center py-16 bg-black text-white"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.3 }}
-        transition={{ duration: 0.8, delay: 0.2 }}
+        viewport={{ once: false }}
+        transition={{ duration: 0.8 }}
       >
         <motion.h2
-          className="text-3xl font-bold mb-4"
+          className="text-3xl font-subtitle font tracking-wider font-medium mb-4"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
+          viewport={{ once: false, amount: 0.5 }}
+          transition={{ duration: 0.6 }}
         >
           Our new <span className="text-bright-red">INTERNSPACE</span>
         </motion.h2>
 
         <motion.p
-          className="max-w-4xl mx-auto text-gray-300 mb-12"
+          className="max-w-4xl mx-auto text-gray-300 mb-12 font-poppins"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.6, delay: 0.6 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.6 }}
         >
           One space where interns can access brochures, topper's highlights,
           updates, certificates, and offer letters and more, all organized for
           smooth and easy use.
         </motion.p>
 
-        <div className="flex flex-wrap justify-center gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-4 items-center mx-auto max-w-6xl w-full p-2 justify-center gap-6">
           {items.map((item, idx) => {
-            const delay = 0.2 + idx * 0.1; // Staggered delay for each item
+            const delay = 0.2 + idx * 0.1;
             return (
               <motion.a
                 key={idx}
@@ -187,7 +193,7 @@ const Career = () => {
                 className="max-w-60 border border-white rounded-xl p-8 hover:-translate-y-2 transition duration-200 w-full text-center"
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true, amount: 0.5 }}
+                viewport={{ once: false }}
                 transition={{
                   duration: 0.5,
                   delay: delay,
@@ -195,9 +201,9 @@ const Career = () => {
                 }}
               >
                 <span className="mx-auto">{item.icon}</span>
-                <h3 className="text-lg font-semibold">
-                  {item.title}{" "}
-                  <span className="text-yellow-400 text-xl font-normal font-logo">
+                <h3 className="text-lg font-semibold text-center flex flex-col items-center justify-center">
+                  <span>{item.title}</span>
+                  <span className="text-yellow-400 text-lg md:text-2xl font-subtitle font tracking-wider font-medium">
                     {item.highlight}
                   </span>
                 </h3>
