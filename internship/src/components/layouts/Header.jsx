f13dc58d164525b65838df121fe6cd425f8fbc85
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-11-29 21:06:41 +0530

    Header Ui Update

--- internship/src/components/layouts/Header.jsx
+++ internship/src/components/layouts/Header.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { getImage } from "../../utils/get-image";
 import { Link } from "react-router-dom";
-import logo from "../../assets/icons/internship.jpg";
+import logo from "../../assets/icons/anicomic.png";
 import { FaBars } from "react-icons/fa";
 import { motion } from "framer-motion";
 import MobileNav from "./MobileNav";
+import { FaCaretDown } from "react-icons/fa6";
 
 const Header = () => {
   const navLink = [
@@ -17,6 +17,7 @@ const Header = () => {
       title: "Showcase",
       link: false,
       dropDown: [
+        { title: "Studio", link: "https://anicomic.vercel.app/" },
         { title: "Character", link: "/character" },
         { title: "Comic", link: "/comic" },
         { title: "Podcast", link: "/podcast" },
@@ -36,17 +37,20 @@ const Header = () => {
       title: "Internship",
       link: false,
       dropDown: [
-        { title: "Internship", link: "/" },
+        { title: "Internship", link: "https://internship.anicomic.in" },
         {
           title: "Intern Portal",
           link: "/downloads",
         },
-        { title: "Registration", link: "/registration" },
+        {
+          title: "Registration",
+          link: "/registration",
+        },
       ],
     },
     {
       title: "Career",
-      link: '/not-available'
+      link: "/not-available",
       // dropDown: [
       //   { title: "Internship", link: "/" },
       //   {
@@ -76,36 +80,68 @@ const Header = () => {
     },
   ];
 
-  const [title, setTitle] = useState();
   const [display, setDisplay] = useState();
   const handleMenu = () => {
     setDisplay(!display);
   };
 
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
   return (
     <header className="max-w-screen w-full sticky top-0 z-20 flex justify-between mx-auto bg-black md:bg-gradient-to-t from-0% to-black">
       <Link
-        onClick={() => setTitle("Anicomic")}
         href="https://anicomic.in"
         to={"https://anicomic.in"}
-        className="min-h-10 p-1 rounded-lg text-white flex items-center text-lg my-1"
+        className="min-h-10 p-1 rounded-lg text-white flex items-center text-lg my-1 mx-3"
       >
-        <img src={logo} alt="logo" className="w-35  h-full object-cover " />
+        <img src={logo} alt="logo" className="w-35 h-13 object-cover " />
       </Link>
 
-      <nav className="min-w-1/3 w-max text-white flex justify-between ml-auto items-center gap-2 mr-5">
-        {navLink.map((nav, i) => {
+      <nav className="min-w-1/3 w-max text-white flex justify-between ml-auto items-center gap-2 mr-5 z-10">
+        {navLink.map((nav, idx) => {
           return (
-            <Link
-              onClick={() =>
-                setTitle(nav.title === "Career" ? "Internship" : nav.title)
-              }
-              key={i}
-              to={nav.link}
-              className=" hover:text-red-700 font-semibold hidden md:block to-10% transition-colors duration-300 p-2 px-3"
-            >
-              {nav.title}
-            </Link>
+            <>
+              {nav.link ? (
+                <Link
+                  key={idx}
+                  to={nav.link || location.href}
+                  className="transition-colors group duration-300 text-center text-semibold flex gap-1 relative items-center text-sm max-w-80 w-full font-bold px-2"
+                >
+                  {nav.title}
+                  {nav.dropDown && <FaCaretDown className="ml-auto" />}
+                </Link>
+              ) : (
+                <div
+                  key={idx}
+                  className="transition-colors group duration-300 text-center text-semibold flex gap-1 relative items-center text-sm max-w-80 w-full font-bold px-2 cursor-pointer"
+                >
+                  {nav.title}
+                  {nav.dropDown && <FaCaretDown className="ml-auto" />}
+                  <div className="flex flex-col w-[150%] transition-all duration-150 h-0 overflow-hidden absolute top-5 py-2 pt-4 group-hover:h-auto left-auto bg-black/40 gap-1">
+                    {nav.dropDown &&
+                      nav.dropDown.map((drop, i) => {
+                        return (
+                          <motion.a
+                            href={drop.link}
+                            key={i}
+                            initial={{ x: -40, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            exit={{ x: -10, opacity: 0.4 }}
+                            transition={{ ease: "easeIn", delay: 0.12 * i }}
+                            className="w-full p-2 cursor-pointer my-1 text-left hover:bg-bright-red/40"
+                          >
+                            <Link className="w-full min-w-full" to={drop.link}>
+                              {drop?.title}
+                            </Link>
+                          </motion.a>
+                        );
+                      })}
+                  </div>
+                </div>
+              )}
+            </>
           );
         })}
         <MobileNav
